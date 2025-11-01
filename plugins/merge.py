21375d921530e7a297e054be82fe2d7705b5b353
xiaojianzheng <1272209235@qq.com>
2025-11-01 18:10:44 +0800

    update

--- plugins/merge.py
+++ plugins/merge.py
@@ -1,6 +1,8 @@
 import os
 import json
-import re
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+all_json = os.path.join(script_dir, "all.json")
 
 
 def to_camel_case(snake_str):
@@ -14,7 +16,7 @@ def to_camel_case(snake_str):
 
 def merge_json_files():
     # 获取当前目录下所有的json文件
-    json_files = [f for f in os.listdir(".") if f.endswith(".json")]
+    json_files = [f for f in os.listdir(script_dir) if f.endswith(".json")]
 
     merged_data = {}
 
@@ -23,9 +25,11 @@ def merge_json_files():
         file_name = os.path.splitext(json_file)[0]
         # 如果文件名包含横杠，则转换为驼峰格式
         camel_case_name = to_camel_case(file_name)
+        if camel_case_name == "all":
+            continue
 
         # 打开并读取json文件内容
-        with open(json_file, "r", encoding="utf-8") as file:
+        with open(os.path.join(script_dir, json_file), "r", encoding="utf-8") as file:
             try:
                 data = json.load(file)
                 merged_data[camel_case_name] = data
@@ -33,11 +37,12 @@ def merge_json_files():
                 print(f"Warning: {json_file} is not a valid JSON file, skipping.")
 
     # 输出合并后的 JSON 对象到一个新的文件
-    with open("all.json", "w", encoding="utf-8") as output_file:
+    with open(all_json, "w", encoding="utf-8") as output_file:
         json.dump(merged_data, output_file, ensure_ascii=False, indent=4)
 
-    print("All JSON files have been merged into 'merged_output.json'.")
+    print("All JSON files have been merged into 'all.json'.")
 
 
 if __name__ == "__main__":
+    os.path.exists(all_json) and os.remove(all_json)
     merge_json_files()
